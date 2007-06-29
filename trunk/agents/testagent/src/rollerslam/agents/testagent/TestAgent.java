0f3f22303202e9ed274ff466ffa176bd27fb0b78
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-06-29 20:53:55 +0000

    Uploading rmitest agent

---
+++ trunk/agents/testagent/src/rollerslam/agents/testagent/TestAgent.java
@@ -0,0 +1,72 @@
+/*
+ *  Rollerslam - The Global Fusion Sport of the Third Millennium
+ *  Copyright (C) 2007  ORCAS Research Group
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  http://code.google.com/p/rollerslam
+ *  
+ */
+
+package rollerslam.agents.testagent;
+
+import java.rmi.RemoteException;
+import java.util.Random;
+
+import rollerslam.agents.Agent;
+import rollerslam.infrastructure.server.Message;
+import rollerslam.infrastructure.server.TestMessage;
+
+/**
+ * Simple agent for testing purposes.
+ * 
+ * @author maas
+ */
+public class TestAgent extends Thread implements Agent {
+
+	private Agent server;
+	
+	public TestAgent(Agent server) {
+		this.server = server;
+	}
+	
+	/* (non-Javadoc)
+	 * @see rollerslam.agents.Agent#sendPerception(rollerslam.infrastructure.server.Message)
+	 */
+	public void sendPerception(Message m) throws RemoteException {
+
+	}
+	
+	public void run() {
+		Random r = new Random();
+		
+		while(true) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			int msg = r.nextInt(1000);
+			
+			System.out.println("SENDING MSG " + msg);
+			try {
+				server.sendPerception(new TestMessage(msg));
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+}
