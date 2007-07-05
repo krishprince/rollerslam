ec2e47d8577b46b846a9750024e2f160dc7ef6c7
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-07-05 18:08:15 +0000

    implementing annotations facility and tictactoe example

---
+++ trunk/environment/src/tictactoe/agents/TicTacToeAgent.java
@@ -0,0 +1,19 @@
+package tictactoe.agents;
+
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import rollerslam.infrastructure.annotations.agent;
+import rollerslam.infrastructure.annotations.message;
+import tictactoe.environment.BoardState;
+import tictactoe.environment.Marker;
+
+public @agent interface TicTacToeAgent extends Serializable, Remote {
+	@message void boardStateChanged(BoardState state) throws RemoteException;
+	@message void youWon() throws RemoteException;
+	@message void youLost() throws RemoteException;
+	@message void wrongMove() throws RemoteException;
+	@message void gameStarted(Marker you) throws RemoteException;
+}
