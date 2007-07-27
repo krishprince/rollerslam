481c1912ba78b5a88c4562f8abeeeae2de7d0397
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-07-27 00:29:33 +0000

    solving bugs adding display

---
+++ interfaces/src/tictactoe/StateMessage.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import rollerslam.infrastructure.server.Message;
+
+/**
+ * Carries the current game state
+ * 
+ * @author maas
+ */
+public class StateMessage implements Message {
+
+	public BoardState state;
+		
+}
