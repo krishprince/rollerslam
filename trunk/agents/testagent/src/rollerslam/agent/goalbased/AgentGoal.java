40f4a3c9077e884229d911601a16d5644e09d95d
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-03 13:31:55 +0000

    just kidding

--- trunk/agents/testagent/src/rollerslam/agent/goalbased/AgentGoal.java
+++ trunk/agents/testagent/src/rollerslam/agent/goalbased/AgentGoal.java
@@ -3,5 +3,7 @@ package rollerslam.agent.goalbased;
 public enum AgentGoal {
 	JOIN_GAME,
 	WAIT_JOIN_GAME,
-	GO_TO_CENTER;
+	GO_TO_CENTER,
+	GO_TO_GOAL
+	;
 }
--- trunk/agents/testagent/src/rollerslam/agent/goalbased/AgentGoalUpdater.java
+++ trunk/agents/testagent/src/rollerslam/agent/goalbased/AgentGoalUpdater.java
@@ -1,5 +1,8 @@
 package rollerslam.agent.goalbased;
 
+import rollerslam.environment.model.Player;
+import rollerslam.environment.model.World;
+import rollerslam.environment.model.actions.DashAction;
 import rollerslam.infrastructure.agent.goalbased.GoalBasedEnvironmentStateModel;
 import rollerslam.infrastructure.agent.goalbased.GoalUpdateComponent;
 
@@ -14,9 +17,40 @@ public class AgentGoalUpdater implements GoalUpdateComponent {
 			}
 		} else if (model.currentGoal == AgentGoal.WAIT_JOIN_GAME) {
 			if (model.gameStarted) {
+				System.out.println("GOING TO CENTER");
 				model.currentGoal = AgentGoal.GO_TO_CENTER;
 			}			
+		} else if (model.currentGoal == AgentGoal.GO_TO_CENTER) { 
+			Player me = getMeFromModel(model);
+			
+			double mex = me.sx / 1000.0;
+			double mey = me.sy / 1000.0;
+			
+			double d = Math.sqrt(mex*mex + mey*mey);
+			
+			System.out.println("DISTANCE TO CENTER: " + d);
+			
+			if (d < 10.0) {
+				System.out.println("GOING TO GOAL");
+				model.currentGoal = AgentGoal.GO_TO_GOAL;				
+			}
 		}
 	}
 
+	private Player getMeFromModel(AgentWorldModel model) {
+		for (Player player : ((World)model.environmentStateModel).playersA) {
+			if (player.id == model.myID) {
+				return player;
+			}
+		}
+
+		for (Player player : ((World)model.environmentStateModel).playersB) {
+			if (player.id == model.myID) {
+				return player;
+			}
+		}
+
+		return null;
+	}
+	
 }
