1bc77882a9a8338a376da3ef62fa13d5e8f83acb
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-06 19:27:45 +0000

    Inclusão dos esqueletos das ações

--- trunk/environment/src/rollerslam/environment/JavaActionInterpretationComponent.java
+++ trunk/environment/src/rollerslam/environment/JavaActionInterpretationComponent.java
@@ -6,10 +6,22 @@ import java.util.Hashtable;
 import rollerslam.environment.model.Player;
 import rollerslam.environment.model.PlayerTeam;
 import rollerslam.environment.model.World;
-import rollerslam.environment.model.actions.DashAction;
+import rollerslam.environment.model.actions.arm.Antitackle;
+import rollerslam.environment.model.actions.ArmAction;
+import rollerslam.environment.model.actions.arm.CatchAction;
+import rollerslam.environment.model.actions.leg.DashAction;
+import rollerslam.environment.model.actions.leg.HitAction;
 import rollerslam.environment.model.actions.JoinGameAction;
+import rollerslam.environment.model.actions.leg.KickAction;
+import rollerslam.environment.model.actions.LegAction;
+import rollerslam.environment.model.actions.arm.ReleaseAction;
+import rollerslam.environment.model.actions.voice.SendMsgAction;
+import rollerslam.environment.model.actions.arm.TackleAction;
+import rollerslam.environment.model.actions.arm.ThrowAction;
+import rollerslam.environment.model.actions.VoiceAction;
 import rollerslam.environment.model.perceptions.GameStartedPerception;
 import rollerslam.environment.model.utils.Vector;
+import rollerslam.environment.model.utils.Fact;
 import rollerslam.infrastructure.agent.Agent;
 import rollerslam.infrastructure.agent.Message;
 import rollerslam.infrastructure.agent.automata.ActionInterpretationComponent;
@@ -42,11 +54,88 @@ public class JavaActionInterpretationComponent implements ActionInterpretationCo
 		p.ax = (int) nax;
 		p.ay = (int) nay;
 	}
+	
+	private void hit(World w, Player p, Vector vet){
+		//Body
+	}
 
+	private void kick(World w, Player p, Vector vet){
+		//Body
+	}
+	
+	private void antitackle(World w, Player p){
+		//Body
+	}
+	
+	private void catchA(World w, Player p){
+		//Body
+	}
+	
+	private void release(World w, Player p){
+		//Body
+	}
+	
+	private void tackle(World w, Player p){
+		//Body
+	}
+	
+	private void throwA(World w, Player p, Vector a){
+		//Body
+	}
+	
+	private void sendMsg(World w, Player p, Fact f){
+		//Body
+	}	
+		
 	public void processAction(EnvironmentStateModel w, Message m) {
-		if (m instanceof DashAction) {
-			DashAction mt = (DashAction) m;
-			this.dash((World)w, playersMap.get(mt.sender), mt.acceleration);
+		//Actions of Leg
+		if (m instanceof LegAction) {
+			if (m instanceof DashAction) {
+				//Dash
+				DashAction mt = (DashAction) m;
+				this.dash((World)w, playersMap.get(mt.sender), mt.acceleration);
+			} else if(m instanceof KickAction) {
+				if(m instanceof HitAction){
+					//Hit the ball
+					HitAction mt = (HitAction) m;
+					this.hit((World)w, playersMap.get(mt.sender), mt.acceleration);
+				}else{
+					//Kick the ball
+					KickAction mt = (KickAction) m;
+					this.kick((World)w, playersMap.get(mt.sender), mt.acceleration);
+				}
+			}
+		//Actions of Arm
+		} else if (m instanceof ArmAction) {
+			if(m instanceof Antitackle){
+				//Antitackle Action
+				Antitackle mt = (Antitackle) m;
+				this.antitackle((World)w, playersMap.get(mt.sender));
+			}else if(m instanceof CatchAction){
+				//Catch the ball
+				CatchAction mt = (CatchAction) m;
+				this.catchA((World)w, playersMap.get(mt.sender));
+			}else if(m instanceof ReleaseAction){
+				//Release the ball
+				ReleaseAction mt = (ReleaseAction) m;
+				this.release((World)w, playersMap.get(mt.sender));
+			}else if(m instanceof TackleAction){
+				//Tackle another player
+				TackleAction mt = (TackleAction) m;
+				this.tackle((World)w, playersMap.get(mt.sender));
+			}else if(m instanceof ThrowAction){
+				//Throw the ball
+				ThrowAction mt = (ThrowAction) m;
+				this.throwA((World)w, playersMap.get(mt.sender), mt.acceleration);
+			}
+		//Actions of Voice
+		} else if(m instanceof VoiceAction){
+			if(m instanceof SendMsgAction){
+				//Send the message ("Say")
+				SendMsgAction mt = (SendMsgAction) m;
+				this.sendMsg((World)w, playersMap.get(mt.sender), mt.subject);
+			}
+		//Player Join to Game
 		} else if (m instanceof JoinGameAction) {
 			JoinGameAction mt = (JoinGameAction) m;
 			this.joinWorld((World)w, mt.sender, mt.team);			
