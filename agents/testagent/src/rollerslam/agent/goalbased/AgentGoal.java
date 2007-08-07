3d67e9cbb2ea2aeb78505883de3784f2ce11480f
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-07 12:55:10 +0000

    Implementing Actions Catch and Tackle.

--- agents/testagent/src/rollerslam/agent/goalbased/AgentGoal.java
+++ agents/testagent/src/rollerslam/agent/goalbased/AgentGoal.java
@@ -3,7 +3,9 @@ package rollerslam.agent.goalbased;
 public enum AgentGoal {
 	JOIN_GAME,
 	WAIT_JOIN_GAME,
-	GO_TO_CENTER,
-	GO_TO_GOAL
+	GO_TO_BALL,
+	GO_TO_GOAL,
+	CATCH_BALL,
+        TACKLE_PLAYER
 	;
 }
