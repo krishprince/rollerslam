a77fe8476114524c361f85b3ed8afb5ee3f871f1
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-07 15:30:29 +0000

    Correction of the player's creation.

--- infrastructure/src_interfaces/rollerslam/infrastructure/agent/Agent.java
+++ infrastructure/src_interfaces/rollerslam/infrastructure/agent/Agent.java
@@ -1,7 +1,9 @@
 package rollerslam.infrastructure.agent;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Agent extends Remote {
-
+    public int getID() throws RemoteException;
+    public void setID(int id) throws RemoteException;
 }
