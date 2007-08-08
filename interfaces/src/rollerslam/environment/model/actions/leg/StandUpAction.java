9cc929784ad377741a4399ae1610fbc27312ae8e
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-08 15:55:12 +0000

    .

--- interfaces/src/rollerslam/environment/model/actions/leg/StandUpAction.java
+++ interfaces/src/rollerslam/environment/model/actions/leg/StandUpAction.java
@@ -1,9 +1,9 @@
 package rollerslam.environment.model.actions.leg;
 
 import rollerslam.environment.model.actions.LegAction;
-import rollerslam.environment.model.utils.Vector;
 import rollerslam.infrastructure.agent.Agent;
 
+@SuppressWarnings("serial")
 public class StandUpAction extends LegAction {
 	public StandUpAction(Agent a){
 		super(a);
