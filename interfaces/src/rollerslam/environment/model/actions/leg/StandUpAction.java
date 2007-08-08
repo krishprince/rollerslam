4777478d599c8776349f5f23034799d6dae02f93
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-08 13:44:13 +0000

    Modifications

---
+++ interfaces/src/rollerslam/environment/model/actions/leg/StandUpAction.java
@@ -0,0 +1,11 @@
+package rollerslam.environment.model.actions.leg;
+
+import rollerslam.environment.model.actions.LegAction;
+import rollerslam.environment.model.utils.Vector;
+import rollerslam.infrastructure.agent.Agent;
+
+public class StandUpAction extends LegAction {
+	public StandUpAction(Agent a){
+		super(a);
+	}	
+}
