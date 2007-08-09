02073b24b34a3ad8a22b5687e69109d185157e27
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-09 11:29:52 +0000

    solving agent id bug creating class with all the important simulation constants creating class to start a whole agent team at once

--- trunk/display/src/rollerslam/display/gui/sprite/SpriteKind.java
+++ trunk/display/src/rollerslam/display/gui/sprite/SpriteKind.java
@@ -18,13 +18,13 @@ public enum SpriteKind {
     
     //read team
     RED_PLAYER("rollerslam/display/gui/resources/pnb.png"),
-    RED_PLAYER_WITH_BALL("rollerslam/display/gui/resources/pwb.png"),
+    RED_PLAYER_WITH_BALL("rollerslam/display/gui/resources/pnb.png"),
     RED_PLAYER_GROUNDED("rollerslam/display/gui/resources/pnb.png"),
     
     //blue team
-    BLUE_PLAYER("rollerslam/display/gui/resources/pnb.png"),
+    BLUE_PLAYER("rollerslam/display/gui/resources/pwb.png"),
     BLUE_PLAYER_WITH_BALL("rollerslam/display/gui/resources/pwb.png"),
-    BLUE_PLAYER_GROUNDED("rollerslam/display/gui/resources/pnb.png"),
+    BLUE_PLAYER_GROUNDED("rollerslam/display/gui/resources/pwb.png"),
     
     //misc
     BALL("rollerslam/display/gui/resources/ball.png"),
