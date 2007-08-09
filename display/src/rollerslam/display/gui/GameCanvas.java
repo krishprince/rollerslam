f2a4bfee2c67a2002c295c944063bfe943d78160
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-09 11:29:52 +0000

    solving agent id bug creating class with all the important simulation constants creating class to start a whole agent team at once

--- display/src/rollerslam/display/gui/GameCanvas.java
+++ display/src/rollerslam/display/gui/GameCanvas.java
@@ -5,7 +5,6 @@ package rollerslam.display.gui;
 
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 
@@ -14,10 +13,11 @@ import javax.swing.JLabel;
 import rollerslam.display.gui.mvc.Model;
 import rollerslam.display.gui.sprite.SpriteKind;
 import rollerslam.display.gui.sprite.SpriteStore;
-import rollerslam.environment.model.Fact;
-import rollerslam.environment.model.OutTrack;
 import rollerslam.environment.model.Player;
+import rollerslam.environment.model.SimulationSettings;
 import rollerslam.environment.model.World;
+import rollerslam.environment.model.actions.voice.SendMsgAction;
+import rollerslam.infrastructure.agent.Message;
 
 /**
  *
@@ -81,8 +81,10 @@ public class GameCanvas extends Canvas {
 
                         ss.getSprite(SpriteKind.BALL).draw(g, translatex(world.ball.s.x), translatey(world.ball.s.y));
                         
-                        for (Fact fact : world.saidMessages) {
-							MessageHandler.scheduleForExhibition(fact);
+                        for (Message message : world.actions) {
+                        	if (message instanceof SendMsgAction) {
+                        		MessageHandler.scheduleForExhibition(((SendMsgAction)message).subject);
+                        	}
 						}
                         
                         messagesLabel.setText(MessageHandler.getCurrentMessage());
@@ -102,12 +104,12 @@ public class GameCanvas extends Canvas {
     }
 
     private int translatex(int sx) {
-        return ((sx + OutTrack.WIDTH / 2) * 782) / OutTrack.WIDTH;
+        return ((sx + SimulationSettings.OUTTRACK_WIDTH / 2) * 782) / SimulationSettings.OUTTRACK_WIDTH;
     	//return (sx * 764) / World.WIDTH / 2;
     }
 
     private int translatey(int sy) {
-        return ((sy + OutTrack.HEIGHT / 2) * 582) / OutTrack.HEIGHT;
+        return ((sy + SimulationSettings.OUTTRACK_HEIGHT / 2) * 582) / SimulationSettings.OUTTRACK_HEIGHT;
     	//return (sy * 564) / World.HEIGHT / 2; 
     }
 
