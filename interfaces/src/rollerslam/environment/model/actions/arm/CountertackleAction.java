686557d23824092398b01df5bce069936733eb96
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-09 21:12:01 +0000

    All actions implemented, but no all actions used for agent (hit and sendmsg not used). All constants in SimulationSettings Class.

--- interfaces/src/rollerslam/environment/model/actions/arm/CountertackleAction.java
+++ interfaces/src/rollerslam/environment/model/actions/arm/CountertackleAction.java
@@ -4,8 +4,8 @@ import rollerslam.environment.model.actions.ArmAction;
 import rollerslam.infrastructure.agent.Agent;
 
 @SuppressWarnings("serial")
-public class Antitackle extends ArmAction {
-	public Antitackle(Agent a){
+public class CountertackleAction extends ArmAction {
+	public CountertackleAction(Agent a){
 		super(a);
 	}
 }
