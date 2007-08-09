c290069111bbf8d6972bea0b3c9cdc280253ddd2
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-09 11:30:01 +0000

    solving agent id bug creating class with all the important simulation constants creating class to start a whole agent team at once

--- trunk/infrastructure/src_interfaces/rollerslam/infrastructure/agent/Agent.java
+++ trunk/infrastructure/src_interfaces/rollerslam/infrastructure/agent/Agent.java
@@ -4,6 +4,4 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Agent extends Remote {
-    public int getID() throws RemoteException;
-    public void setID(int id) throws RemoteException;
 }
