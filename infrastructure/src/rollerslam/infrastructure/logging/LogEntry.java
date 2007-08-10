50c40253d384a1954bc897d34c9d58b5d4378945
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-10 13:16:34 +0000

    adding log to facades

---
+++ infrastructure/src/rollerslam/infrastructure/logging/LogEntry.java
@@ -0,0 +1,11 @@
+package rollerslam.infrastructure.logging;
+
+import rollerslam.infrastructure.agent.Agent;
+
+public class LogEntry {
+
+	public int cycle;
+	public Agent agent;
+	public LogData data;
+	
+}
