2a16884eefc8f982e13faca5a0ea25f840eab7bd
weslei <weslei@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-14 19:22:40 +0000

    Improved object serializing in database. Now the log records the world and log information as it is (the real objects).

---
+++ logging/src/orcas/helpers/SerializationHelper.java
@@ -0,0 +1,54 @@
+/*
+ * SerializationHelper.java
+ *
+ * Created on 14/08/2007, 16:01:59
+ *
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package orcas.helpers;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import rollerslam.environment.model.World;
+
+/**
+ *
+ * @author Weslei
+ */
+public final class SerializationHelper {
+
+    private SerializationHelper() {
+    }
+
+    public static String object2String(Object o) {
+        String res = null;
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(o);
+            oos.flush();
+            res = baos.toString();
+            oos.close();
+        } catch (Exception err) {
+            throw new RuntimeException("Error on object2string.");
+        }
+        return res;
+    }
+
+    public static Object string2Object(String st) {
+        Object o = null;
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(st.getBytes());
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            o = ois.readObject();
+            bais.close();
+        } catch (Exception err) {
+            throw new RuntimeException("Error on string2object.");
+        }
+        return o;
+    }
+}
