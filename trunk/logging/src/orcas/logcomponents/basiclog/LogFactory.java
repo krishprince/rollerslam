fdb037be208aa345d6d1bbf67a0e6ebb3fde6ae7
weslei <weslei@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-16 15:27:03 +0000

    Fixed log recoding/playing implementation to get properties as parameter and not reading a property file.

--- trunk/logging/src/orcas/logcomponents/basiclog/LogFactory.java
+++ trunk/logging/src/orcas/logcomponents/basiclog/LogFactory.java
@@ -1,53 +1,58 @@
 /*
  * LogFactory.java
- * 
+ *
  */
 
 package orcas.logcomponents.basiclog;
+
+import java.util.Properties;
 
 /**
  *
  * @author Weslei
  */
 public class LogFactory {
-    
+
     private static LogFactory instance;
     private AbstractLog log;
 
-    public LogFactory() {
-        try {
-            java.util.Properties p = new java.util.Properties();
-            p.load(LogFactory.class.getResourceAsStream("basiclogproperties.properties"));
-            
-            String logClass = p.getProperty("concrete.log.class");
-            if (logClass == null || "".equals(logClass.trim())) {
-                logClass = "orcas.logcomponents.basiclog.SystemConsoleLog";
-            }
-            log = (AbstractLog) Class.forName(logClass).newInstance();
-            
-            log.init(p);
-            
-            int logLevel = Integer.parseInt(p.getProperty("log.level"));
-            
-            log.setLogLevel(LogLevel.get(logLevel));
-        } catch (Exception ex) {
-            throw new RuntimeException("Error to instantiate log system... \n\n" + ex);
+    private LogFactory(Properties props) throws Exception {
+        String logClass = props.getProperty("concrete.log.class");
+        if (logClass == null || "".equals(logClass.trim())) {
+            logClass = "orcas.logcomponents.basiclog.SystemConsoleLog";
         }
+        log = (AbstractLog) Class.forName(logClass).newInstance();
+
+        log.init(props);
+
+        int logLevel = Integer.parseInt(props.getProperty("log.level"));
+
+        log.setLogLevel(LogLevel.get(logLevel));
     }
-    
-    public static LogFactory getInstance() {        
+
+    public static LogFactory getInstance(Properties props) {
         if (instance == null) {
-            synchronized(LogFactory.class) {
+            synchronized (LogFactory.class) {
                 if (instance == null) {
-                    instance = new LogFactory();
+                    try {
+                        instance = new LogFactory(props);
+                    } catch (Exception err) {
+                        throw new RuntimeException("Error initializing logFactory. Details: " + err, err);
+                    }
                 }
             }
         }
-        return instance;
+        return getInstance();
     }
     
+    public static LogFactory getInstance() {
+        if (instance == null) {
+            throw new RuntimeException("LogFactory not initialized. Call getInstance(props) at first usage.");
+        }
+        return instance;
+    }
+
     public Log getLog() {
         return log;
     }
-
 }
--- trunk/logplayer/src/rollerslam/logplayer/impl/LogPlayingServiceImpl.java
+++ trunk/logplayer/src/rollerslam/logplayer/impl/LogPlayingServiceImpl.java
@@ -34,11 +34,7 @@ public class LogPlayingServiceImpl implements LogPlayingService {
     private Connection conn;
     private String user = "sa";
     private String password = "";
-
-
-    private String readWorldInCycleSQL = "SELECT winfo FROM t_log WHERE cycle = ? AND NOT winfo IS NULL";
-    private String readAdditionalDataInCycleSQL = "SELECT ainfo FROM t_log WHERE cycle = ? AND NOT ainfo IS NULL";
-    private String readMessagesInCycleSQL = "SELECT msg FROM t_log WHERE cycle = ? AND NOT msg IS NULL";
+    
     private String readLogForAgentSQL = "SELECT thelog FROM t_log WHERE aid = ?";
 
     public LogPlayingServiceImpl() {
@@ -65,7 +61,7 @@ public class LogPlayingServiceImpl implements LogPlayingService {
 
             System.out.println("read " + totalCycles + " registers");
         } catch (Exception err) {
-            throw new RuntimeException("Error loading simDescription. Details: " + err);
+            throw new RuntimeException("Error loading simDescription. Details: " + err, err);
         }
     }
 
@@ -92,86 +88,26 @@ public class LogPlayingServiceImpl implements LogPlayingService {
         }
     }
 
-    public World getWorld() {
-        World w = null;
+    public LogEntry getLogForAgent(Integer agId) {
+        LogEntry le = null;
         try {
-            PreparedStatement ps = conn.prepareStatement(readWorldInCycleSQL);
-            ps.setInt(1, this.currentCycle);
-
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new RuntimeException("No world info for the cycle " + currentCycle);
-            }
-            w = (World) SerializationHelper.string2Object(rs.getString(1));
-            
-            ps.close();
-        } catch (Exception ex) {
-            throw new RuntimeException("Error reading object. Details: " + ex);
-        }
-        return w;
-    }
-
-    public List<Message> getMessages() {
-        List<Message> ms = new ArrayList<Message>();
-        try {
-            PreparedStatement ps = conn.prepareStatement(readMessagesInCycleSQL);
-            ps.setInt(1, this.currentCycle);
+            PreparedStatement ps = conn.prepareStatement(readLogForAgentSQL);
+            ps.setInt(1, agId);
 
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
-                ms.add((Message) SerializationHelper.string2Object(rs.getString(1)));
+                le = (LogEntry)SerializationHelper.string2Object(rs.getString(1));
             }
             
             ps.close();
         } catch (Exception ex) {
-            throw new RuntimeException("Error reading object. Details: " + ex);
+            throw new RuntimeException("Error reading object. Details: " + ex, ex);
         }
-        return ms;
-    }
-
-    public Object getAdditionalData() {
-        Object o = null;
-        try {
-            PreparedStatement ps = conn.prepareStatement(readAdditionalDataInCycleSQL);
-            ps.setInt(1, this.currentCycle);
-
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new RuntimeException("No additional data info for the cycle " + currentCycle);
-            }
-            o = SerializationHelper.string2Object(rs.getString(1));
-            
-            ps.close();
-        } catch (Exception ex) {
-            throw new RuntimeException("Error reading object. Details: " + ex);
-        }
-        return o;
-    }
-
-    public LogEntry getLogForAgent(Integer agId) {
-        LogEntry le = null;
-//        try {
-//            PreparedStatement ps = conn.prepareStatement(readLogForAgentSQL);
-//            ps.setInt(1, agId);
-//
-//            ResultSet rs = ps.executeQuery();
-//
-//            while (rs.next()) {
-//                les.add((LogEntry) SerializationHelper.string2Object(rs.getString(1)));
-//            }
-//            
-//            ps.close();
-//        } catch (Exception ex) {
-//            throw new RuntimeException("Error reading object. Details: " + ex);
-//        }
         return le;
     }
 
     public static void main(String[] args) {
-        LogPlayingServiceImpl lpi = new LogPlayingServiceImpl();
-        lpi.load("file:///c:/temp/testrollerslam_20070814_1613");
-        lpi.setCurrentCycle(3);
-        System.out.println(((String[]) lpi.getAdditionalData())[1]);
+
     }
 }
