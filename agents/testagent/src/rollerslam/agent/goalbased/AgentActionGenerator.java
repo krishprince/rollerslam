976f6a27c253f1c3ed9b0f80c73d3a2c1e40d87e
brenomachado <brenomachado@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-18 02:09:17 +0000

    Include Logging. Environment and agents send actions

--- agents/testagent/src/rollerslam/agent/goalbased/AgentActionGenerator.java
+++ agents/testagent/src/rollerslam/agent/goalbased/AgentActionGenerator.java
@@ -2,6 +2,7 @@ package rollerslam.agent.goalbased;
 
 import java.rmi.RemoteException;
 
+import rollerslam.environment.RollerslamEnvironmentAgent;
 import rollerslam.environment.model.Player;
 import rollerslam.environment.model.PlayerTeam;
 import rollerslam.environment.model.World;
@@ -16,8 +17,12 @@ import rollerslam.environment.model.actions.arm.ThrowAction;
 import rollerslam.environment.model.actions.arm.CountertackleAction;
 import rollerslam.infrastructure.agent.Agent;
 import rollerslam.infrastructure.agent.Message;
+import rollerslam.infrastructure.agent.StateMessage;
 import rollerslam.infrastructure.agent.automata.EnvironmentStateModel;
 import rollerslam.infrastructure.agent.automata.ModelBasedBehaviorStrategyComponent;
+import rollerslam.infrastructure.server.ServerFacade;
+import rollerslam.infrastructure.server.ServerFacadeImpl;
+import rollerslam.logging.AgentActionLogEntry;;
 
 public class AgentActionGenerator implements
 		ModelBasedBehaviorStrategyComponent {
@@ -30,56 +35,69 @@ public class AgentActionGenerator implements
 
 	public Message computeAction(EnvironmentStateModel w) {
 		AgentWorldModel model = (AgentWorldModel) w;
+		Message m = null;
 
 		if (model.currentGoal == AgentGoal.JOIN_GAME) {
 			model.joinMessageSent = true;
-			return new JoinGameAction(remoteThis, model.myTeam);
+			m = new JoinGameAction(remoteThis, model.myTeam);
 		} else if (model.currentGoal == AgentGoal.WAIT_JOIN_GAME) {
 
 		} else if (model.currentGoal == AgentGoal.GO_TO_BALL) {
 			Player me = model.getMe();
 
-			return new DashAction(remoteThis, me.world.ball.s.subtract(me.s));
+			m = new DashAction(remoteThis, me.world.ball.s.subtract(me.s));
 		} else if (model.currentGoal == AgentGoal.GO_TO_GOAL) {
 			Player me = model.getMe();
 
 			World world = (World) model.environmentStateModel;
 
 			if (me.team == PlayerTeam.TEAM_A) {
-				return new DashAction(remoteThis, world.goalB.s.subtract(me.s));
+				m = new DashAction(remoteThis, world.goalB.s.subtract(me.s));
 			} else {
-				return new DashAction(remoteThis, world.goalA.s.subtract(me.s));
+				m = new DashAction(remoteThis, world.goalA.s.subtract(me.s));
 			}
 
 		} else if (model.currentGoal == AgentGoal.CATCH_BALL) {
-			return new CatchAction(remoteThis);
+			m = new CatchAction(remoteThis);
 		} else if (model.currentGoal == AgentGoal.TACKLE_PLAYER) {
-			return new TackleAction(remoteThis);
+			m = new TackleAction(remoteThis);
 		} else if (model.currentGoal == AgentGoal.STAND_UP) {
-			return new StandUpAction(remoteThis);
+			m = new StandUpAction(remoteThis);
 		} else if (model.currentGoal == AgentGoal.THROW_BALL) {
 			Player me = model.getMe();
 			World world = (World) model.environmentStateModel;
 
 			if (me.team == PlayerTeam.TEAM_A) {
-				return new ThrowAction(remoteThis, world.goalB.s.subtract(me.s));
+				m = new ThrowAction(remoteThis, world.goalB.s.subtract(me.s));
 			} else {
-				return new ThrowAction(remoteThis, world.goalA.s.subtract(me.s));
+				m = new ThrowAction(remoteThis, world.goalA.s.subtract(me.s));
 			}
         } else if(model.currentGoal == AgentGoal.KICK_BALL){
         	Player me = model.getMe();
 			World world = (World)model.environmentStateModel;
 				        	
         	if(me.team == PlayerTeam.TEAM_A){
-				return new KickAction(remoteThis, world.goalB.s.subtract(me.s));
+        		m = new KickAction(remoteThis, world.goalB.s.subtract(me.s));
 			}else{
-				return new KickAction(remoteThis, world.goalA.s.subtract(me.s));
+				m = new KickAction(remoteThis, world.goalA.s.subtract(me.s));
 			}
         } else if(model.currentGoal == AgentGoal.COUNTER_TACKLE){
-        	return new CountertackleAction(remoteThis);			
+        	m = new CountertackleAction(remoteThis);			
         }
 
-		return null;
+		try {
+			ServerFacade facade = ServerFacadeImpl.getInstance();
+			AgentActionLogEntry envLog = new AgentActionLogEntry(
+					((World) model.environmentStateModel).currentCycle, model.myID, m);
+
+			facade.getLogRecordingService().addEntry(envLog);
+
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return m;
+		}
+		
+		return m;
 	}
 
 
