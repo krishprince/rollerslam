e5f00c9419c68d1ce0c9f7f917aa92ee84ca90b5
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-21 00:32:41 +0000

    NA

--- agents/testagent/src/rollerslam/agent/sentinel/SentinelAgentActionGenerator.java
+++ agents/testagent/src/rollerslam/agent/sentinel/SentinelAgentActionGenerator.java
@@ -2,26 +2,14 @@ package rollerslam.agent.sentinel;
 
 import java.rmi.RemoteException;
 
-import rollerslam.agent.goalbased.AgentGoal;
-import rollerslam.agent.goalbased.AgentWorldModel;
-import rollerslam.environment.model.Player;
-import rollerslam.environment.model.PlayerTeam;
 import rollerslam.environment.model.World;
-import rollerslam.environment.model.actions.JoinGameAction;
 import rollerslam.environment.model.actions.VoiceAction;
-import rollerslam.environment.model.actions.arm.CatchAction;
-import rollerslam.environment.model.actions.arm.CountertackleAction;
-import rollerslam.environment.model.actions.arm.TackleAction;
-import rollerslam.environment.model.actions.arm.ThrowAction;
-import rollerslam.environment.model.actions.leg.DashAction;
-import rollerslam.environment.model.actions.leg.KickAction;
-import rollerslam.environment.model.actions.leg.StandUpAction;
 import rollerslam.infrastructure.agent.Agent;
 import rollerslam.infrastructure.agent.Message;
 import rollerslam.infrastructure.agent.automata.EnvironmentStateModel;
 import rollerslam.infrastructure.agent.automata.ModelBasedBehaviorStrategyComponent;
-import rollerslam.infrastructure.server.ServerFacade;
-import rollerslam.infrastructure.server.ServerFacadeImpl;
+import rollerslam.infrastructure.client.ClientFacade;
+import rollerslam.infrastructure.client.ClientFacadeImpl;
 import rollerslam.logging.AgentActionLogEntry;
 
 public class SentinelAgentActionGenerator implements
@@ -44,7 +32,7 @@ public class SentinelAgentActionGenerator implements
 			m = new VoiceAction(remoteThis);
 		} 
 		try {
-			ServerFacade facade = ServerFacadeImpl.getInstance();
+			ClientFacade facade = ClientFacadeImpl.getInstance();
 			
 			int cycle = 0;
 			if ((World) model.environmentStateModel != null)
