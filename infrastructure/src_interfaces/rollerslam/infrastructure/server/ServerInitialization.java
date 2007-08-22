295251e4a7155a87362a85bce0600be2abe5a11e
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-22 17:15:16 +0000

    separating log from simulation

--- infrastructure/src_interfaces/rollerslam/infrastructure/server/ServerInitialization.java
+++ infrastructure/src_interfaces/rollerslam/infrastructure/server/ServerInitialization.java
@@ -1,6 +1,7 @@
 package rollerslam.infrastructure.server;
 
 import rollerslam.infrastructure.agent.automata.AutomataAgent;
+import rollerslam.infrastructure.logging.LogRecordingService;
 
 
 public interface ServerInitialization {
@@ -15,4 +16,6 @@ public interface ServerInitialization {
 	 * @throws Exception
 	 */
 	void init(int port, AutomataAgent environmentAgent) throws Exception;
+
+    void init(int port, AutomataAgent environmentAgent, LogRecordingService logRecordingService) throws Exception; 
 }
