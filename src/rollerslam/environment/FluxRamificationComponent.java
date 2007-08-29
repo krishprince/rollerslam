b347f47eae21c139e5ce8129ecc484646cbf8bc5
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-29 23:06:58 +0000

    integrating to eclipse prolog

--- src/rollerslam/environment/FluxRamificationComponent.java
+++ src/rollerslam/environment/FluxRamificationComponent.java
@@ -1,33 +1,44 @@
 package rollerslam.environment;
 
-import java.io.IOException;
-
+import rollerslam.environment.model.World;
+import rollerslam.environment.visitor.JavaPrologWorldVisitor;
+import rollerslam.environment.visitor.PrologJavaWorldVisitor;
+import rollerslam.environment.visitor.SampleJavaPrologWorldVisitor;
 import rollerslam.infrastructure.agent.automata.EnvironmentStateModel;
 import rollerslam.infrastructure.agent.automata.RamificationComponent;
 
 import com.parctechnologies.eclipse.CompoundTerm;
 import com.parctechnologies.eclipse.EclipseConnection;
-import com.parctechnologies.eclipse.EclipseException;
 
 public class FluxRamificationComponent implements RamificationComponent {
 
 	private EclipseConnection eclipse;
+	private JavaPrologWorldVisitor javaPrologVisitor;
+	private PrologJavaWorldVisitor prologJavaVisitor;
 	
 	public FluxRamificationComponent(EclipseConnection eclipse) {
 		this.eclipse = eclipse;
+		this.javaPrologVisitor = new SampleJavaPrologWorldVisitor();
+		this.prologJavaVisitor = new SamplePrologJavaWorldVisitor();
 	}
 	
 	public void processRamifications(EnvironmentStateModel world) {
-	    try {
-			CompoundTerm result = eclipse.rpc("runRamifications(X)");
-		} catch (EclipseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String query = "processRamifications(["+javaPrologVisitor.getPrologRepresentation((World) world)+"], R)";
 
+//		System.out.println("query: "+query);
+		
+		CompoundTerm ret;
+		try {
+			ret = eclipse.rpc(query);
+			
+//			System.out.println("result: "+ret);
+			
+			System.out.println(ret.arg(1).equals(ret.arg(2)));
+			prologJavaVisitor.updateWorldRepresentation((World)world, ret.arg(2));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
 	}
 
 }
