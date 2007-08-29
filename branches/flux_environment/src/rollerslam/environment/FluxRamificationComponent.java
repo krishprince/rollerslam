65b7818ba7a445fdbe38226417e86a3bdd330738
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-08-29 12:58:41 +0000

    adding flux stuff

---
+++ branches/flux_environment/src/rollerslam/environment/FluxRamificationComponent.java
@@ -0,0 +1,13 @@
+package rollerslam.environment;
+
+import rollerslam.infrastructure.agent.automata.EnvironmentStateModel;
+import rollerslam.infrastructure.agent.automata.RamificationComponent;
+
+public class FluxRamificationComponent implements RamificationComponent {
+
+	public void processRamifications(EnvironmentStateModel world) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
