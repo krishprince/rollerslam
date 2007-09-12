33faba0897c7eb691c1d79e3e5b4151ab8c57eee
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-09-12 22:42:07 +0000

    adding toString

--- trunk/interfaces/src/rollerslam/environment/model/strategy/DefineRoleFact.java
+++ trunk/interfaces/src/rollerslam/environment/model/strategy/DefineRoleFact.java
@@ -9,4 +9,9 @@ public class DefineRoleFact implements Serializable {
 	public PlayerPosition position = null;
 	public AgentRole role = null;
 	public Vector posCoord = null;
+	
+	public String toString() {
+		return "Your position is " + position + " your role is " + role
+				+ " and your coord is " + posCoord;
+	}
 }
