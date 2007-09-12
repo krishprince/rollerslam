7e61e474a68b41e6b438fa0a5d108a2b6043b3ad
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-09-12 22:42:07 +0000

    adding toString

--- interfaces/src/rollerslam/environment/model/strategy/InitializationFact.java
+++ interfaces/src/rollerslam/environment/model/strategy/InitializationFact.java
@@ -5,4 +5,7 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class InitializationFact implements Serializable {
 
+	public String toString() {
+		return "Hi! How are you?";
+	}
 }
