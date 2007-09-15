1835ba3cedb7d1752873a283f36cc3c12b7ac03f
weslei <weslei@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2007-09-15 22:46:05 +0000

    LogRecording BICT implementation and models.

--- trunk/infrastructure/src/rollerslam/infrastructure/logging/LogRecordingServiceImpl.java
+++ trunk/infrastructure/src/rollerslam/infrastructure/logging/LogRecordingServiceImpl.java
@@ -15,7 +15,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Properties;
-
 import orcas.logcomponents.basiclog.Log;
 import orcas.logcomponents.basiclog.LogFactory;
 import rollerslam.infrastructure.settings.GeneralSettings;
@@ -29,23 +28,29 @@ import rollerslam.infrastructure.settings.GeneralSettingsImpl;
 public class LogRecordingServiceImpl implements LogRecordingService {
 
     private static LogRecordingService instance;
-    
-    private Log log = null;
-    
+
+    protected Log log = null;
+
     private LogRecordingServiceImpl() {
-        try {
-            Properties p = new Properties();
-            GeneralSettings gs = GeneralSettingsImpl.getInstance();
-            p.setProperty("concrete.log.class", (String) gs.getSetting(GeneralSettings.LOG_CLASS));
-
-            p.setProperty("log.level", "0");//not used propety, but is required for the background log component
-            p.setProperty("db.url", (String) gs.getSetting(GeneralSettings.DB_URL));
-
-            log = LogFactory.getInstance(p).getLog();
-        } catch (RemoteException ex) {
-            throw new RuntimeException("Error initalizing log service. Details: " + ex, ex);
-        }
+        this(null);
     }
+
+    protected LogRecordingServiceImpl(Properties p) {
+        if (p == null) {
+            try {
+                p = new Properties();
+                GeneralSettings gs = GeneralSettingsImpl.getInstance();
+                p.setProperty("concrete.log.class", (String) gs.getSetting(GeneralSettings.LOG_CLASS));
+
+                p.setProperty("log.level", "0"); //not used propety, but is required for the background log component
+                p.setProperty("db.url", (String) gs.getSetting(GeneralSettings.DB_URL));
+            } catch (RemoteException ex) {
+                throw new RuntimeException("Error initalizing log service. Details: " + ex, ex);
+            }
+        }
+        log = LogFactory.getInstance(p).getLog();
+    }
+
 
     public void addEntry(LogEntry entry) throws RemoteException {
         log.log(entry);
@@ -62,17 +67,17 @@ public class LogRecordingServiceImpl implements LogRecordingService {
         instance = new LogRecordingServiceImpl();
         return getInstance();
     }
-    
-    public static void main(String[] str) throws Exception {
-    	LogRecordingServiceImpl.init();
-    	LogRecordingService logRecordingService = LogRecordingServiceImpl.getInstance();
 
-    	LocateRegistry.createRegistry(1099);
-    	
-    	Remote logExp = UnicastRemoteObject.exportObject(logRecordingService, 0);
-    	Naming.rebind(LogRecordingService.class.getSimpleName(), logExp);
-    	
-    	System.out.println("Logging service exported!");
+    public static void main(String[] str) throws Exception {
+        LogRecordingServiceImpl.init();
+        LogRecordingService logRecordingService = LogRecordingServiceImpl.getInstance();
+
+        LocateRegistry.createRegistry(1099);
+
+        Remote logExp = UnicastRemoteObject.exportObject(logRecordingService, 0);
+        Naming.rebind(LogRecordingService.class.getSimpleName(), logExp);
+
+        System.out.println("Logging service exported!");
 
         Object o = new Object();
 
