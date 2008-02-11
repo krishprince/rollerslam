0bf22faa9aac1f7c446fe17767a2876d996cd3fc
maurelio1234 <maurelio1234@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2008-02-11 09:15:10 +0000

    deleting old code from trunk

--- trunk/infrastructure/src/rollerslam/infrastructure/agent/automata/ModelBasedBehaviorStrategyComponent.java
+++
@@ -1,7 +0,0 @@
-package rollerslam.infrastructure.agent.automata;
-
-import rollerslam.infrastructure.agent.Message;
-
-public interface ModelBasedBehaviorStrategyComponent {
-	Message computeAction(EnvironmentStateModel w);
-}
--- trunk/infrastructure/src/rollerslam/infrastructure/eclipse/EclipsePrologHandler.java
+++
@@ -1,7 +0,0 @@
-package rollerslam.infrastructure.eclipse;
-
-import com.parctechnologies.eclipse.EclipseConnection;
-
-public interface EclipsePrologHandler {
-	public EclipseConnection getEclipseConnection();
-}
