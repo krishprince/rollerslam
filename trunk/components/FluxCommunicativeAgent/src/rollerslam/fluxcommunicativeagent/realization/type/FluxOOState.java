925109c1795291336862682d0d0aaff43ae2c8c8
doleron@gmail.com <doleron@gmail.com@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2008-02-22 20:21:32 +0000

    criação de classe para realizar toString human-friendly para CompoundTerm and Atom

--- trunk/components/FluxCommunicativeAgent/src/rollerslam/fluxcommunicativeagent/realization/type/FluxOOState.java
+++ trunk/components/FluxCommunicativeAgent/src/rollerslam/fluxcommunicativeagent/realization/type/FluxOOState.java
@@ -1,8 +1,10 @@
 package rollerslam.fluxcommunicativeagent.realization.type;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import rollerslam.agent.communicative.specification.type.object.ObjectState;
+import rollerslam.fluxinferenceengine.realization.type.EclipsePrologFluent;
 import rollerslam.fluxinferenceengine.specification.type.Fluent;
 
 public class FluxOOState extends ObjectState {
@@ -14,6 +16,22 @@ public class FluxOOState extends ObjectState {
 	}
 	
 	public String toString() {
-		return "" + fluents;
+		StringBuffer retorno = new StringBuffer();
+		Iterator<Fluent> iterator = this.fluents.iterator();
+		for(int i = 0;i<this.fluents.size();i++){
+			Fluent fluent = iterator.next();
+			String item = "";
+			if(fluent instanceof EclipsePrologFluent){
+				EclipsePrologFluent eclipsePrologFluent = (EclipsePrologFluent)fluent;
+				item = ToStringPrinterUtility.toString(eclipsePrologFluent.term);
+			} else {
+				item = fluent.toString();
+			}
+			if(i<this.fluents.size()-1){
+				item = item + ", ";
+			}
+			retorno.append(item);
+		}
+		return retorno.toString();
 	}
 }
