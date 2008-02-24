4e90bcadc3bdf7fa818e550233892a79a88d9b82
doleron@gmail.com <doleron@gmail.com@5a0e5a4b-0f31-0410-82c4-3903895abd97>
2008-02-24 16:38:22 +0000

    correção do import

--- trunk/components/TopLevel/src/rollerslam/toplevel/realization/service/RollerslamImpl.java
+++ trunk/components/TopLevel/src/rollerslam/toplevel/realization/service/RollerslamImpl.java
@@ -1,6 +1,8 @@
 package rollerslam.toplevel.realization.service;
 import rollerslam.agent.communicative.specification.service.CommunicativeAgent;
+import rollerslam.agent.communicative.specification.type.CommunicativeAgentID;
 import rollerslam.common.datatype.PlayerTeam;
+import rollerslam.display.realization.service.DisplayAgent;
 import rollerslam.fluxcommunicativeagent.realization.type.FluxAgentID;
 import rollerslam.gamephysics.realization.service.GamePhysicsAgent;
 import rollerslam.infrastructure.realization.service.SimulationInfrastructureImpl;
@@ -22,15 +24,15 @@ public class RollerslamImpl extends Rollerslam {
 	public RollerslamImpl() throws Exception {
 			infrastructure = new SimulationInfrastructureImpl();
 			
-			//AgentID displayID = new CommunicativeAgentID("display");
+			AgentID displayID = new CommunicativeAgentID("display");
 			AgentID gamePhysicsID = new FluxAgentID("gamePhysics");
 			AgentID playerID = new FluxAgentID("player");
 			
-			//Agent displayConnector = infrastructure.connectAgent(displayID);
+			Agent displayConnector = infrastructure.connectAgent(displayID);
 			Agent gamePhysicsConnector = infrastructure.connectAgent(gamePhysicsID);						
 			Agent playerConnector = infrastructure.connectAgent(playerID);
 			
-			//display = new DisplayAgent(displayConnector, gamePhysicsID, 50);
+			display = new DisplayAgent(displayConnector, gamePhysicsID, 50);
 			gamePhysics = new GamePhysicsAgent(gamePhysicsConnector, 1, 50);
 			player = new PlayerAgent(playerConnector, 1, PlayerTeam.TEAM_A, 50);
 			
