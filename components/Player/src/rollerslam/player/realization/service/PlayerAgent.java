c0509416281dcf837c6f3658771d1247331557fa
doleron <doleron@gmail.com>
2008-02-25 20:00:06 +0000

    removido referencia para PlayerTeam deprecated

--- components/Player/src/rollerslam/player/realization/service/PlayerAgent.java
+++ components/Player/src/rollerslam/player/realization/service/PlayerAgent.java
@@ -3,24 +3,23 @@ package rollerslam.player.realization.service;
 
 import java.io.File;
 
-import com.parctechnologies.eclipse.Atom;
-
-import rollerslam.common.datatype.PlayerTeam;
 import rollerslam.fluxcommunicativeagent.realization.service.FluxCommunicativeAgent;
 import rollerslam.fluxcommunicativeagent.realization.type.FluxOID;
 import rollerslam.infrastructure.specification.service.Agent;
+
+import com.parctechnologies.eclipse.Atom;
 
 public class PlayerAgent extends FluxCommunicativeAgent {
 	
 	private static final String ADDRESS_FLUX_FILE = PlayerAgent.class.getResource("player.pl").getFile();
 
-	public PlayerAgent(Agent port, long cycleLength, PlayerTeam team, int playerID) throws Exception {
+	public PlayerAgent(Agent port, long cycleLength, String team, int playerID) throws Exception {
 		super(port, new File(ADDRESS_FLUX_FILE), "player", cycleLength);
 		
 		FluxOID me = new FluxOID(new Atom("me"));
 
 		super.declareFAtom(me, "id", ""+playerID);
-		super.declareFAtom(me, "team", team.toString());
+		super.declareFAtom(me, "team", team);
 		super.declareFAtom(me, "senseCycle", "true");
 	}
 
