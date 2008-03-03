d1606af92c93d4f8f1c6e5f32700d17e81e5764e
doleron <doleron@gmail.com>
2008-03-03 02:57:09 +0000

    encapsulating State fields

--- components/FluxInferenceEngine/src/rollerslam/fluxinferenceengine/specification/type/State.java
+++ components/FluxInferenceEngine/src/rollerslam/fluxinferenceengine/specification/type/State.java
@@ -3,17 +3,26 @@ package rollerslam.fluxinferenceengine.specification.type;
 import java.util.Arrays;
 
 public class State {
-	public Fluent[] fluents;
+
+	private Fluent[] fluents;
+
+	public Fluent[] getFluents() {
+		return fluents;
+	}
+
+	public void setFluents(Fluent[] fluents) {
+		this.fluents = fluents;
+	}
 
 	public State(Fluent[] fluents) {
 		super();
 		this.fluents = fluents;
 	}
-	
+
 	public State() {
 		this(new Fluent[0]);
 	}
-	
+
 	public String toString() {
 		return Arrays.toString(fluents);
 	}
